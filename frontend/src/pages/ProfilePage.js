2cb379c92597305e7e1829962b7fa714ddfd8c24
Krtin Shet <krtin71@gmail.com>
2020-11-03 13:34:10 +0530

    replaced the a tags to link in profile page

--- frontend/src/pages/ProfilePage.js
+++ frontend/src/pages/ProfilePage.js
@@ -1,70 +1,98 @@
-import React,{useState,useEffect} from 'react'
-import {Container,Nav,Tab}from 'react-bootstrap'
+import React, { useState, useEffect } from 'react';
+import { Container, Nav, Tab } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import Settings from '../components/Settings';
+import Reviews from '../components/Reviews';
+import Bookings from '../components/Bookings';
 
-import ReactDOM from 'react-dom';
+function ProfilePage({ tab } = 'settings') {
+	const [sideNav, setsideNav] = useState(false);
+	const sideNavtoggle = () => {
+		const a = sideNav ? false : true;
+		setsideNav(a);
+	};
 
-import Settings from '../components/Settings'
-import Reviews from '../components/Reviews'
-import Bookings from '../components/Bookings'
+	const [screen, setscreen] = useState('settings');
+	useEffect(() => {
+		setscreen(tab);
+	}, [screen]);
 
-function ProfilePage({tab}='settings') {
-    const [sideNav,setsideNav]=useState(false);
-    const sideNavtoggle=()=>{
-        const a = sideNav?false:true;
-        setsideNav(a);
-    }
+	const handlescreen = (screen = 'settings') => {
+		switch (screen) {
+			case 'settings':
+				return <Settings />;
+			case 'reviews':
+				return <Reviews />;
+			case 'bookings':
+				return <Bookings />;
+			default:
+				return <div> this is a wrong page</div>;
+		}
+	};
+	return (
+		<Tab.Container defaultActiveKey='settings'>
+			<Container id='wrapper' className={`${sideNav ? 'toggled' : ''}`}>
+				<aside id='sidebar-wrapper'>
+					<Nav className='sidebar-nav' as='ul'>
+						<Nav.Item as='li'>
+							<LinkContainer to='/settings'>
+								<Nav.Link
+									className={`sidenav-icon ${
+										tab === 'settings' ? 'active' : ''
+									}`}
+								>
+									<i className='fa fa-user-cog' />
+									Settings
+								</Nav.Link>
+							</LinkContainer>
+						</Nav.Item>
 
-    const [screen , setscreen]=useState('settings')
-    useEffect(() => {
-    setscreen(tab)
-    }, [screen])
-    
-    const handlescreen=(screen = 'settings')=>{
-        switch (screen) {
-            case 'settings':
-           return <Settings/>;
-            case 'reviews':
-           return <Reviews/>;
-            case 'bookings':
-           return <Bookings/>;
-            default:
-            return <div> this is a wrong page</div>;
-        }
-    }
-    return (
-    <Tab.Container defaultActiveKey="settings">
-    <Container id='wrapper' className={`${sideNav? 'toggled':''}`} >
-        <aside id="sidebar-wrapper">
-          <Nav className="sidebar-nav" as="ul">
-            <Nav.Item as="li" >
-              <Nav.Link as="a" href='/settings' className={`sidenav-icon ${(tab === 'settings')? 'active':''}`}><i className="fa fa-user-cog" />Settings</Nav.Link>
-            </Nav.Item>
-            <Nav.Item as="li">
-              <Nav.Link as="a"  href='/reviews' className={`sidenav-icon ${(tab === 'reviews')? 'active':''}`}><i className="fa fa-star" />Reviews</Nav.Link>
-            </Nav.Item>
-            <Nav.Item as="li">
-              <Nav.Link as="a"  href='/bookings' className={`sidenav-icon ${(tab === 'bookings')? 'active':''}`}><i class="fa fa-shopping-bag"></i>Bookings</Nav.Link>
-            </Nav.Item>
+						<Nav.Item as='li'>
+							<LinkContainer to='/reviews'>
+								<Nav.Link
+									className={`sidenav-icon ${
+										tab === 'reviews' ? 'active' : ''
+									}`}
+								>
+									<i className='fa fa-star' />
+									Reviews
+								</Nav.Link>
+							</LinkContainer>
+						</Nav.Item>
 
-          </Nav>
-        </aside>
-        <div id="navbar-wrapper">
-          <Nav className="navbar">
-              <div onClick={sideNavtoggle} className={`${sideNav? '':'navbar-inverse'}`}>
-                <span className="sidenav-icon" ><i className="fa fa-angle-double-right" /></span>
-            </div>
-          </Nav>
-        </div>
-        <section id="content-wrapper" className="overflow-auto">
-            <div className="col-lg-12">
-               <Tab.Content>
-                 {handlescreen(screen)}
-               </Tab.Content>
-            </div>
-        </section>
-      </Container>
-      </Tab.Container>
-)
+						<Nav.Item as='li'>
+							<LinkContainer to='/bookings'>
+								<Nav.Link
+									className={`sidenav-icon ${
+										tab === 'bookings' ? 'active' : ''
+									}`}
+								>
+									<i class='fa fa-shopping-bag'></i>Bookings
+								</Nav.Link>
+							</LinkContainer>
+						</Nav.Item>
+					</Nav>
+				</aside>
+				<div id='navbar-wrapper'>
+					<Nav className='navbar'>
+						<div
+							onClick={sideNavtoggle}
+							className={`${sideNav ? '' : 'navbar-inverse'}`}
+						>
+							<span className='sidenav-icon'>
+								<i className='fa fa-angle-double-right' />
+							</span>
+						</div>
+					</Nav>
+				</div>
+				<section id='content-wrapper' className='overflow-auto'>
+					<div className='col-lg-12'>
+						<Tab.Content>{handlescreen(screen)}</Tab.Content>
+					</div>
+				</section>
+			</Container>
+		</Tab.Container>
+	);
 }
 
-export default ProfilePage
+export default ProfilePage;
