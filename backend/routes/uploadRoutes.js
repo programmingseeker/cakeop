9a542ac40465f4e2a9683dfbd413cf14cde0fed2
Gaurav-S-Rao <gauravsrao.dev@gmail.com>
2020-11-16 21:17:34 +0530

    Contact page

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -1,32 +1,31 @@
-const path = require('path');
-const express = require('express');
-const uploadImage = require('./../utils/uploadImage.js');
-const { restrictTo } = require('./../controllers/authController.js');
-const { updateMe } = require('./../controllers/UserController.js');
-
+const path = require("path");
+const express = require("express");
+const uploadImage = require("./../utils/uploadImage.js");
+const { restrictTo } = require("./../controllers/authController.js");
+const { updateMe } = require("./../controllers/UserController.js");
 const router = express.Router();
-const publicCakeImg = path.join(__dirname, 'public/img/cake');
-const publicUserImg = path.join(__dirname, 'public/img/user');
+const publicCakeImg = path.join(__dirname, "public/img/cake");
+const publicUserImg = path.join(__dirname, "public/img/user");
 
 const cakeImageUpload = uploadImage(publicCakeImg);
 const userImageUpload = uploadImage(publicUserImg);
 
 router.post(
-	'/',
-	restrictTo('admin'),
-	cakeImageUpload.any('images', 5),
-	(req, res) => {
-		const data = [];
-		req.files.map((file) => data.push(file.filename.toString()));
-		res.json({ images: data });
-	}
+  "/",
+  restrictTo("admin"),
+  cakeImageUpload.any("images", 5),
+  (req, res) => {
+    const data = [];
+    req.files.map((file) => data.push(file.filename.toString()));
+    res.json({ images: data });
+  }
 );
 
 router.post(
-	'/user',
-	restrictTo('user'),
-	userImageUpload.single('profileImage', 1),
-	updateMe
+  "/user",
+  restrictTo("user"),
+  userImageUpload.single("profileImage", 1),
+  updateMe
 );
 
 module.exports = router;
