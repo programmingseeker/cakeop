7c6818b22cb1f5d52e7ebf7bad2a2e78f1a73408
Gaurav-S-Rao <gauravsrao.dev@gmail.com>
2020-11-23 00:26:27 +0530

    Implemeted Navbar for mobile devices

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,114 +1,122 @@
-import React, { useEffect, useState } from 'react';
-import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
-import { Route } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { LinkContainer } from 'react-router-bootstrap';
-import { logout } from './../actions/userActions';
-import FormContent from './FormContent';
+import React, { useEffect, useState } from "react";
+import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
+import { Route } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { LinkContainer } from "react-router-bootstrap";
+import { logout } from "./../actions/userActions";
+import FormContent from "./FormContent";
 
 const Header = () => {
-	const dispatch = useDispatch();
-	const { user } = useSelector((state) => state.auth);
-	const [showModal, setshowModal] = useState(false);
-	const showModalHandler = () => {
-		setshowModal(true);
-	};
-	const hideModalHandler = () => {
-		setshowModal(false);
-	};
-	const onClickLogoutHandler = () => {
-		dispatch(logout());
-	};
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.auth);
+  const [showModal, setshowModal] = useState(false);
+  const showModalHandler = () => {
+    setshowModal(true);
+  };
+  const hideModalHandler = () => {
+    setshowModal(false);
+  };
+  const onClickLogoutHandler = () => {
+    dispatch(logout());
+  };
 
-	return (
-		<Navbar
-			collapseOnSelect
-			expand='lg'
-			bg='white'
-			fixed='top'
-			className='shadow'
-		>
-			<Container className='text-center'>
-				<LinkContainer to='/'>
-					<Navbar.Brand>
-						<img
-							alt='Logo'
-							src='/img/icons/logo.svg'
-							className='d-inline-block align-middle'
-						/>
-						{'  '}
-						<span className='align-middle h2 font-weight-bold'>
-							CakeOp
-						</span>
-					</Navbar.Brand>
-				</LinkContainer>
-				<Navbar.Toggle aria-controls='#navbarSupportedContent' />
-				<Navbar.Collapse id='navbarSupportedContent'>
-					<Nav className='ml-auto text-center'>
-						<LinkContainer to='#about' className='px-3'>
-							<Nav.Link>
-								<i className='fas fa-users fa-lg px-2'></i>
-								About Us
-							</Nav.Link>
-						</LinkContainer>
+  const HeaderContentHandler = () => {
+    return (
+      <Nav className="ml-auto text-center">
+        <LinkContainer to="#about" className="px-3">
+          <Nav.Link>
+            <i className="fas fa-users fa-lg px-2"></i>
+            About Us
+          </Nav.Link>
+        </LinkContainer>
 
-						<LinkContainer to='/contactus' className='px-3'>
-							<Nav.Link>
-								<i className='fas fa-phone-alt px-2'></i>
-								Contact Us
-							</Nav.Link>
-						</LinkContainer>
+        <LinkContainer to="/contactus" className="px-3">
+          <Nav.Link>
+            <i className="fas fa-phone-alt px-2"></i>
+            Contact Us
+          </Nav.Link>
+        </LinkContainer>
 
-						<LinkContainer to='/cart' className='px-3'>
-							<Nav.Link>
-								<i className='fas fa-shopping-cart px-2'></i>
-								Cart
-							</Nav.Link>
-						</LinkContainer>
+        <LinkContainer to="/cart" className="px-3">
+          <Nav.Link>
+            <i className="fas fa-shopping-cart px-2"></i>
+            Cart
+          </Nav.Link>
+        </LinkContainer>
 
-						{user ? (
-							<NavDropdown
-								title={user.username ? user.username : ''}
-								id='username'
-								onClick={hideModalHandler}
-							>
-								<LinkContainer to='/profile'>
-									<NavDropdown.Item>Profile</NavDropdown.Item>
-								</LinkContainer>
+        {user ? (
+          <NavDropdown
+            title={user.username ? user.username : ""}
+            id="username"
+            onClick={hideModalHandler}
+          >
+            <LinkContainer to="/profile">
+              <NavDropdown.Item>Profile</NavDropdown.Item>
+            </LinkContainer>
 
-								<NavDropdown.Item
-									as='button'
-									className='text-decoration-none text-dark'
-									onClick={onClickLogoutHandler}
-								>
-									Log Out
-								</NavDropdown.Item>
-							</NavDropdown>
-						) : (
-							<>
-								<Nav.Link onClick={showModalHandler}>
-									<i className='fas fa-user pr-2'></i>
-									Log In / Sign Up
-								</Nav.Link>
-								{showModal && (
-									<Route
-										render={({ history, location }) => (
-											<FormContent
-												show={showModal}
-												handleClose={hideModalHandler}
-												history={history}
-												location={location}
-											></FormContent>
-										)}
-									/>
-								)}
-							</>
-						)}
-					</Nav>
-				</Navbar.Collapse>
-			</Container>
-		</Navbar>
-	);
+            <NavDropdown.Item
+              as="button"
+              className="text-decoration-none text-dark"
+              onClick={onClickLogoutHandler}
+            >
+              Log Out
+            </NavDropdown.Item>
+          </NavDropdown>
+        ) : (
+          <>
+            <Nav.Link onClick={showModalHandler}>
+              <i className="fas fa-user pr-2"></i>
+              Log In / Sign Up
+            </Nav.Link>
+            {showModal && (
+              <Route
+                render={({ history, location }) => (
+                  <FormContent
+                    show={showModal}
+                    handleClose={hideModalHandler}
+                    history={history}
+                    location={location}
+                  ></FormContent>
+                )}
+              />
+            )}
+          </>
+        )}
+      </Nav>
+    );
+  };
+  return (
+    <Navbar expand="lg" bg="white" fixed="top" className="shadow">
+      <Container className="text-center">
+        <LinkContainer to="/">
+          <Navbar.Brand>
+            <img
+              alt="Logo"
+              src="/img/icons/logo.svg"
+              className="d-inline-block align-middle"
+            />
+            {"  "}
+            <span className="align-middle h2 font-weight-bold">CakeOp</span>
+          </Navbar.Brand>
+        </LinkContainer>
+        <div className="header-content">
+          <HeaderContentHandler />
+        </div>
+
+        <nav className="mobilescreen">
+          <div title="Menu" id="menuToggle">
+            <input type="checkbox" />
+            <span></span>
+            <span></span>
+            <span></span>
+            <ul id="menu">
+              <HeaderContentHandler />
+            </ul>
+          </div>
+        </nav>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default Header;
